bcbd804981cbbb3ce030b4ac986e3b722a82e2a5
gustavocorrealeal <gustavaobjj@hotmail.com>
2022-02-07 20:14:40 -0300

    finalizado

--- javascript/exercicio4.js
+++ javascript/exercicio4.js
@@ -53,5 +53,43 @@ for (let i = 1; i < numbers.length; i ++) {
 
 console.log(maiornumero);
 
+//e-6
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (let i = 0; i < numbers.length; i ++) {
+  if (numbers[i] % 2 !== 0) {
+    resultado += 1;
+  }
+}
+
+if (resultado === 0) {
+  console.log('impar nao encontrado');
+} else {
+  console.log(resultado);
+}
+
+//e-8
+
+let numbers = [];
+
+for (let i = 1; i <= 25; i ++) {
+  numbers.push(i);
+}
+
+console.log(numbers);
+
+//e-9
+
+let numbers = [
+  1,  2,  3,  4,  5,  6,  7,  8,
+  9, 10, 11, 12, 13, 14, 15, 16,
+ 17, 18, 19, 20, 21, 22, 23, 24,
+ 25
+]
+for (let i = 0; i < numbers.length; i ++) {
+  console.log(numbers[i] / 2);
+}
 
 
